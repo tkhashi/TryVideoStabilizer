e5ad969faeb5c5774432a164fe1b332d5c1811b2
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-05-12 02:40:09 +0900

    Mat破棄処理

--- CppStabilizer/Program.cs
+++ CppStabilizer/Program.cs
@@ -1,24 +1,22 @@
 ﻿using OpenCvSharp;
 
-using var cap = new VideoCapture(@"C:\Users\k_tak\Downloads\『Googleフォト』スタビライズ　手ブレ補正前 歩きver. - from YouTube.mp4"); // 動画ファイルを開く
-
-// フレームサイズの取得
-_ = (int)cap.Get(VideoCaptureProperties.FrameWidth);
-_ = (int)cap.Get(VideoCaptureProperties.FrameHeight);
+const string videoPath = @"Resources/demo.mp4";
+using var cap = new VideoCapture(videoPath);
 
 using var cur = new Mat();
 using var curGrey = new Mat();
 using var prev = new Mat();
 using var prevGrey = new Mat();
 
-cap.Read(prev); // 動画から1フレーム読み込む
+cap.Read(prev);
 Cv2.CvtColor(prev, prevGrey, ColorConversionCodes.BGR2GRAY);
 
-const int horizontalBorderCrop = 20; // クロップするボーダーの幅
+// クロップするボーダーの幅
+const int horizontalBorderCrop = 20;
 
 while (true)
 {
-    if (cap.Read(cur) == false) break; // 動画の終了
+    if (cap.Read(cur) == false) break;
 
     Cv2.CvtColor(cur, curGrey, ColorConversionCodes.BGR2GRAY);
 
@@ -26,15 +24,13 @@ while (true)
     var prevCorner2D = Cv2.GoodFeaturesToTrack(prevGrey, 200, 0.01, 30, null, 7, false, 0.04);
     Point2f[] curCorners2D = { };
 
-    var prevCornersMat = new Mat(prevCorner2D.Length, 1, MatType.CV_32FC2, prevCorner2D);
+    using var prevCornersMat = new Mat(prevCorner2D.Length, 1, MatType.CV_32FC2, prevCorner2D);
     // Optical Flowを計算
     Cv2.CalcOpticalFlowPyrLK(prevGrey, curGrey, prevCorner2D, ref curCorners2D, out _, out _);
 
     // 変換行列を計算
-    var curCorner2DMat = new Mat(curCorners2D.Length, 1, MatType.CV_32FC2, curCorners2D);
-    var T = Cv2.EstimateAffinePartial2D(prevCornersMat, curCorner2DMat);
-
-    // 画像を変換
+    using var curCorner2DMat = new Mat(curCorners2D.Length, 1, MatType.CV_32FC2, curCorners2D);
+    using var T = Cv2.EstimateAffinePartial2D(prevCornersMat, curCorner2DMat);
     var cur2 = new Mat();
     Cv2.WarpAffine(cur, cur2, T, cur.Size());
 
@@ -45,7 +41,7 @@ while (true)
     Cv2.Resize(cur2, cur2, cur.Size());
 
     // 変換前後の画像を並べて表示
-    Mat canvas = Mat.Zeros(cur.Rows, cur.Cols * 2 + 10, cur.Type());
+    using Mat canvas = Mat.Zeros(cur.Rows, cur.Cols * 2 + 10, cur.Type());
     cur.CopyTo(new Mat(canvas, new Rect(0, 0, cur.Cols, cur.Rows)));
     cur2.CopyTo(new Mat(canvas, new Rect(cur2.Cols + 10, 0, cur2.Cols, cur.Rows)));
 
@@ -53,4 +49,6 @@ while (true)
 
     Cv2.ImShow("変換前と変換後", canvas);
     Cv2.WaitKey(20);
+
+    cur2.Dispose();
 }
